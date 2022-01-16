4f59376f252d91e6b75db86a8570ed0c03fa78ad
Jonathan Lo <jonathanlo411@gmail.com>
2022-01-15 21:29:09 -0800

    Landing complete | Set up global media and static files

--- songnality/songnality/settings.py
+++ songnality/songnality/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,9 +117,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#...
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- songnality/songnality/urls.py
+++ songnality/songnality/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Pages
 from landing.views import landing
@@ -7,4 +9,4 @@ from landing.views import landing
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', landing, name="landing")
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
